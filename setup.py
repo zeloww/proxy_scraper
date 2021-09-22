90a9b5bf25f959dd35e3a3dc876fb548082b1c7c
Zelow <83988054+zeloww@users.noreply.github.com>
2021-09-22 20:33:05 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+setup(
+    name='proxy_scraper',
+    version='1.0',
+    license='GNU',
+    authors=['zelow'],
+    description='Get any type of proxies in 1 click!',
+    long_description=long_description,
+    keywords=['python', 'py', 'proxy', 'proxies', 'getproxy', 'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
+    packages=['proxy_scraper']
+)
