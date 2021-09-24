29b2c18abfff87e4969644d4cd28cf5101fc96de
Zelow <83988054+zeloww@users.noreply.github.com>
2021-09-24 20:26:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
+import pypandoc
 
-with open('README.md', 'r') as f:
-	long_description = f.read()
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    long_description = long_description.replace("\r","")
+
+except OSError:
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
 
 setup(
     name='proxy_scraper',
-    version='1.0',
-    license='GNU',
+    version='1.2',
+    license='GPLv3+',
     authors=['zelow'],
+    url="https://github.com/zeloww/proxy_scraper",
     description='Get any type of proxies in 1 click!',
     long_description=long_description,
     keywords=['python', 'py', 'proxy', 'proxies', 'getproxy', 'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
