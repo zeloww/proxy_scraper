921d5a6371d401f9ce82a6b1a21c553df61b398f
Zelow <83988054+zeloww@users.noreply.github.com>
2021-09-24 14:14:28 +0200

    Update proxy_scraper.py

--- proxy_scraper/proxy_scraper.py
+++ proxy_scraper/proxy_scraper.py
@@ -5,6 +5,7 @@ class proxies:
     proxies_list = []
 
     def getproxies(dict:bool=False, checker:bool=False, number:int=None, auth:str=None, download:bool=False, type:str="all", country:str="all", timeout:int=1500, ssl:bool=True, anonymity:str="all", format:str="normal", url:str=None, request_url:str="https://google.com/"):
+        proxies_list = []
         i = 0
         
         if ssl:
@@ -61,49 +62,49 @@ class proxies:
             return proxies.proxies_list
 
     def getproxy(dict:bool=False, checker:bool=False, number:int=None, auth:str=None, download:bool=False, type:str="all", country:str="all", timeout:int=1500, ssl:bool=True, anonymity:str="all", format:str="normal", url:str=None, request_url:str="https://google.com/"):
-        if not proxies.proxies_list:
-            i = 0
+        proxies_list = []
+        i = 0
 
-            if ssl:
-                ssl = "yes"
+        if ssl:
+            ssl = "yes"
+        else:
+            ssl = "no"
+
+        if download:
+            download = "getproxies"
+        else:
+            download = "displayproxies"
+
+        if not url:
+            url = f"https://api.proxyscrape.com/?auth={auth}&request={download}&proxytype={type}&timeout={timeout}&ssl={ssl}&anonimity={anonymity}&country={country}&format={format}".replace(",", "&").replace(" ", "") 
+
+        response = request.urlopen(url).read().decode("utf-8").replace("\r", "")
+
+        for proxy in response.splitlines():
+            if number == i:
+                break
+
+            if checker:
+                try:
+                    proxy_handler = request.ProxyHandler({
+                    "https": "http://" + proxy,
+                    "http": "http://" + proxy
+                    })
+
+                    opener = request.build_opener(proxy_handler)
+                    opener.addheaders = [('User-Agent', 'Mozilla/5.0  (Windows NT 6.1; Win64; x64)')]
+                    request.install_opener(opener)
+
+                    response = request.urlopen(request_url, timeout=timeout)
+                    proxy_list.append(proxy)
+
+                except:
+                    pass
+
             else:
-                ssl = "no"
+                proxies.proxies_list.append(proxy)
 
-            if download:
-                download = "getproxies"
-            else:
-                download = "displayproxies"
-
-            if not url:
-                url = f"https://api.proxyscrape.com/?auth={auth}&request={download}&proxytype={type}&timeout={timeout}&ssl={ssl}&anonimity={anonymity}&country={country}&format={format}".replace(",", "&").replace(" ", "") 
-
-            response = request.urlopen(url).read().decode("utf-8").replace("\r", "")
-
-            for proxy in response.splitlines():
-                if number == i:
-                    break
-
-                if checker:
-                    try:
-                        proxy_handler = request.ProxyHandler({
-                        "https": "http://" + proxy,
-                        "http": "http://" + proxy
-                        })
-
-                        opener = request.build_opener(proxy_handler)
-                        opener.addheaders = [('User-Agent', 'Mozilla/5.0  (Windows NT 6.1; Win64; x64)')]
-                        request.install_opener(opener)
-
-                        response = request.urlopen(request_url, timeout=timeout)
-                        proxy_list.append(proxy)
-
-                    except:
-                        pass
-
-                else:
-                    proxies.proxies_list.append(proxy)
-
-                i += 1
+            i += 1
 
         if dict:
             proxies_dict = {
