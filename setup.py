d43d93c1909fe3cbe2e5b6a17b592b40ad1fb006
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-11-23 17:24:12 +0100

    Put pack pypandoc because it's cool.

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
+import pypandoc
 
-with open('README.md', encoding="utf-8") as f:
-    long_description = f.read()
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    long_description = long_description.replace("\r","")
+
+except OSError:
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
 
 setup(
     name='proxy_scraper',
@@ -11,7 +18,6 @@ setup(
     url="https://github.com/zeloww/proxy_scraper",
     description='Get any type of proxies in 1 click!',
     long_description=long_description,
-    keywords=['python', 'py', 'proxy', 'proxies', 'getproxy',
-              'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
+    keywords=['python', 'py', 'proxy', 'proxies', 'getproxy', 'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
     packages=['proxy_scraper']
 )
