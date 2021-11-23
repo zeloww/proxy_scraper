a30027bd6a44720484068bee618dbc56bb4e78e6
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-11-23 10:08:00 +0100

    removing io, can open file without it.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-import io
 from setuptools import setup
 
-with io.open('README.md', encoding="utf-8") as f:
+with open('README.md', encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -12,6 +11,7 @@ setup(
     url="https://github.com/zeloww/proxy_scraper",
     description='Get any type of proxies in 1 click!',
     long_description=long_description,
-    keywords=['python', 'py', 'proxy', 'proxies', 'getproxy', 'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
+    keywords=['python', 'py', 'proxy', 'proxies', 'getproxy',
+              'getproxies', 'proxy-scraper', 'proxies-scraper', 'scraper'],
     packages=['proxy_scraper']
 )
